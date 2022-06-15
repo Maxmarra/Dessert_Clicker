f229ce1e62abf996e94b2a8243fd5dbda7a391ba
Maxmarra <5919669@gmail.com>
2022-06-15 10:45:53 +0300

    1

--- app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2020, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.dessertclicker
 
 import android.content.ActivityNotFoundException
@@ -71,6 +55,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         Log.d(TAG, "onCreated Called")
 
         // Use Data Binding to get reference to the views
@@ -79,8 +64,8 @@ class MainActivity : AppCompatActivity() {
         binding.dessertButton.setOnClickListener {
             onDessertClicked()
         }
-
-        // If there is a savedInstanceState bundle, then you're "restarting" the activity
+        // If there is a savedInstanceState bundle,
+        // then you're "restarting" the activity
         // If there isn't a bundle, then it's a "fresh" start
         if (savedInstanceState != null) {
             revenue = savedInstanceState.getInt(KEY_REVENUE, 0)
@@ -101,27 +86,22 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         Log.d(TAG, "onStart Called")
     }
-
     override fun onResume() {
         super.onResume()
         Log.d(TAG, "onResume Called")
     }
-
     override fun onPause() {
         super.onPause()
         Log.d(TAG, "onPause Called")
     }
-
     override fun onStop() {
         super.onStop()
         Log.d(TAG, "onStop Called")
     }
-
     override fun onDestroy() {
         super.onDestroy()
         Log.d(TAG, "onDestroy Called")
     }
-
     override fun onRestart() {
         super.onRestart()
         Log.d(TAG, "onRestart Called")
@@ -171,12 +151,12 @@ class MainActivity : AppCompatActivity() {
      */
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
+
         Log.d(TAG, "onSaveInstanceState Called")
+
         outState.putInt(KEY_REVENUE, revenue)
         outState.putInt(KEY_DESSERT_SOLD, dessertsSold)
     }
-
-
     /**
      * Menu methods
      */
